c6ae1c70ffa406e552e219ea6275d67a353e50a7
Tu Ninh <tuninh.2411@gmail.com>
2022-06-02 23:39:07 +0700

    Update MainServer url

--- src/constants/server.js
+++ src/constants/server.js
@@ -1,7 +1,7 @@
 export const IMAGE_SERVER = "https://9052-116-109-183-78.ap.ngrok.io"
 export const VIDEO_SERVER = "https://0996-116-109-183-78.ap.ngrok.io"
 
-export const MAIN_SERVER = "http://localhost:8000"
+export const MAIN_SERVER = "http://thevncore-lab.mooo.com:20501"
 export const LOAD_STATE_TIMELINE_RESULT_API = `${MAIN_SERVER}/api/load_ranked_list`
 export const FILTER_API = `${MAIN_SERVER}/api/filter`
 export const SEARCH_API = `${MAIN_SERVER}/api/search`
